1956d697dcf32acc90efbf8fd4b23a84a7871f2d
tna-digital-archiving-jenkins <digitalpreservation@nationalarchives.gov.uk>
2022-04-25 08:24:22 +0000

    Update scalatest to 3.2.12

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -1,7 +1,7 @@
 import sbt._
 
 object Dependencies {
-  lazy val scalaTest = "org.scalatest" %% "scalatest" % "3.2.11"
+  lazy val scalaTest = "org.scalatest" %% "scalatest" % "3.2.12"
   lazy val circeCore = "io.circe" %% "circe-core" % "0.14.1"
   lazy val circeGeneric = "io.circe" %% "circe-generic" % "0.14.1"
   lazy val circeParser = "io.circe" %% "circe-parser" % "0.14.1"
