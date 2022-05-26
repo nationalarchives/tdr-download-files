804ecd858c5cbe01935eff565fab79f377b3360e
tna-digital-archiving-jenkins <digitalpreservation@nationalarchives.gov.uk>
2022-05-26 08:26:55 +0000

    Update mockito-scala to 1.17.7

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -14,7 +14,7 @@ object Dependencies {
   lazy val scalaLogging = "com.typesafe.scala-logging" %% "scala-logging" % "3.9.5"
   lazy val logback = "ch.qos.logback" % "logback-classic" % "1.2.11"
   lazy val logstashLogbackEncoder = "net.logstash.logback" % "logstash-logback-encoder" % "7.2"
-  lazy val mockito = "org.mockito" %% "mockito-scala" % "1.17.5"
+  lazy val mockito = "org.mockito" %% "mockito-scala" % "1.17.7"
   lazy val s3Mock = "io.findify" %% "s3mock" % "0.2.6"
   lazy val elasticMq = "org.elasticmq" %% "elasticmq-server" % "1.3.7"
   lazy val elasticMqSqs = "org.elasticmq" %% "elasticmq-rest-sqs" % "1.3.7"
