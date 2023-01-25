82e9c816915c486284faf0c35194af469bb822d8
tna-digital-archiving-jenkins <digitalpreservation@nationalarchives.gov.uk>
2023-01-25 08:27:41 +0000

    Update decoders-utils, kms-utils, s3-utils, ... to 0.1.70

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -1,7 +1,7 @@
 import sbt._
 
 object Dependencies {
-  private val awsUtilsVersion = "0.1.69"
+  private val awsUtilsVersion = "0.1.70"
 
   lazy val authUtils = "uk.gov.nationalarchives" %% "tdr-auth-utils" % "0.0.108"
   lazy val awsSsm = "software.amazon.awssdk" % "ssm" % "2.19.21"
